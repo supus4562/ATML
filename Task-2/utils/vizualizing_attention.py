b77f70eb56431c191ec291c720d03dc26503a1c1
supus4562 <hamza3140000@gmail.com>
2026-08-14 05:35:21 +0500

    Modification in Task-2

---
+++ Task-2/utils/vizualizing_attention.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import math
+
+NUM_HEADS = 16
+NUM_IMAGES_IN_BATCH = 1
+def visualize_attention(attention_matrix):
+
+    rows = NUM_IMAGES_IN_BATCH * 4 
+    cols = 4
+
+    figs, axs = plt.subplots(rows, cols, figsize=(12,12), layout='constrained')
+    axs = axs.flatten()
+    
+    for j in range(NUM_IMAGES_IN_BATCH):
+        for i in range(NUM_HEADS):
+            plot_idx = j * NUM_HEADS + i
+            attention_1d = attention_matrix[j, i, 0, 1:].detach().cpu().numpy()
+            grid_size = int(math.sqrt(attention_1d.shape[0]))
+            attention_2d = attention_1d.reshape((grid_size, grid_size))
+            axs[plot_idx].imshow(attention_2d)
+            axs[plot_idx].axis('off')
