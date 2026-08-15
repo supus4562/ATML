ab2b31d96a8e3f2ecee2417b0e43406d5420ea29
supus4562 <hamza3140000@gmail.com>
2026-08-15 02:07:21 +0000

    Masked attention

--- Task-2/models/HF_vit_large_patch16_224.py
+++ Task-2/models/HF_vit_large_patch16_224.py
@@ -2,6 +2,6 @@ from transformers import ViTForImageClassification, ViTImageProcessor
 
 def build_vit_HF():
     model_name = 'google/vit-large-patch16-224'
-    processor = ViTImageProcessor.from_pretrained(model_name, do_rescale=False)
+    processor = ViTImageProcessor.from_pretrained(model_name)
     model = ViTForImageClassification.from_pretrained(model_name, attn_implementation='eager')
     return model, processor
